10c9c3a465fde787f36c7717c29941237355dae7
“sadewale72” <adewaleaderoju202@yahoo.com>
2022-07-04 03:00:04 -0700

    My second commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ n - rand() - RAND_MAX / 2;
 lastn = n % 10;
 if (lastn > 5)
 {
-printf("Last digit of %d and is greater than 5\n", n, lastn);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastn);
 }
 else if (lastn == 0)
 {
