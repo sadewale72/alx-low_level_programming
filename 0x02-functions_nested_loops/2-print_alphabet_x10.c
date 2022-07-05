704b0e0bb489b885b7f1a1df1d42d4c55a190084
“sadewale72” <adewaleaderoju202@yahoo.com>
2022-07-05 10:30:51 -0700

    My third commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -> prints the lowercase alphabets
+ */
+void print_alphabet_x10(void);
+{
+int j;
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
