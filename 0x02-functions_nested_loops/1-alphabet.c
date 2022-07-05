5d9e793fcc1fcac380d67b666f52f7e41f33c6b2
“sadewale72” <adewaleaderoju202@yahoo.com>
2022-07-05 10:10:42 -0700

    My second commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,13 @@
 include "main.h"
 /**
- * print_alphabet - a function that prints the alphabet, in lowercase
- *
- * Return: Always 0 (Success)
+ * print_alphabet -> prints the lowercase alphabets
  */
 void print_alphabet(void)
 {
-int n;
-for (n = 'a'; n <= 'z'; n++)
+int j;
+for (j = 'a'; j <= 'z'; j++)
 {
-_putchar(n);
+_putchar(j);
 }
 _putchar('\n');
 }
