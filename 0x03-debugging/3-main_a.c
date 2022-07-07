9b61d0394cb3869fc1009f431ab43a501e227c9d
“sadewale72” <adewaleaderoju202@yahoo.com>
2022-07-07 08:19:55 -0700

    My fourth commit

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -14,17 +14,7 @@ int day;
 int year;
 
 month = 4;
-day = 10;
-year = 1997;
-
-printf("Date: %02d/%02d/%04d\n", month, day, year);
-
-day = convert_day(month, day);
-
-print_remaining_days(month, day, year);
-
-month = 2;
-day = 29;
+day = 01;
 year = 1997;
 
 printf("Date: %02d/%02d/%04d\n", month, day, year);
