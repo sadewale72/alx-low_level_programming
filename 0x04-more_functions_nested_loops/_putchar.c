41b9cad38e842bbe42d854dd5b53e1f510cffc2f
“sadewale72” <adewaleaderoju202@yahoo.com>
2022-07-07 13:08:52 -0700

    My first commit

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -2,7 +2,7 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to prin
+ * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
