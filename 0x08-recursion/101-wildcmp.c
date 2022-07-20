e4d4d383ef07366c33eab52012366e612e7882b3
“sadewale72” <adewaleaderoju202@yahoo.com>
2022-07-20 08:10:56 -0700

    My nineth commit

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,11 +2,11 @@
 
 /**
  * wildcmp - compare strings
- * @sl: pointer to string params
+ * @s1: pointer to string params
  * @s2: pointer to string params
  * Return: 0
  */
-int wildcmp(char *sl, char *s2)
+int wildcmp(char *s1, char *s2)
 {
 if (*s1 == '\0')
 {
